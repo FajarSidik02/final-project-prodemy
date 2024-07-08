c04b6df0476732352c1e4a03bc1cddfe18c3a2f6
Fajar Sidik <jarsidik.002@gmail.com>
2024-07-08 11:01:20 +0700

    first commit

---
+++ test/pageobjects/_base-timetale.js
@@ -0,0 +1,15 @@
+import { browser } from '@wdio/globals'
+
+/**
+* main page object containing all methods, selectors and functionality
+* that is shared across all page objects
+*/
+export default class Page {
+    /**
+    * Opens a sub page of the page
+    * @param path path of the sub page (e.g. /path/to/page.html)
+    */
+    open (path) {
+        return browser.url(`https://timetale.app.prosigmaka.com/app/timetale/login-65fa84538587d32504debb37${path}`)
+    }
+}
