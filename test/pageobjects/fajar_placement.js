08d2b96a08d93215b9bd987e7bc9c35842e893e8
Fajar Sidik <jarsidik.002@gmail.com>
2024-07-11 11:45:24 +0700

    positif test case done -jar

--- test/pageobjects/fajar_placement.js
+++ test/pageobjects/fajar_placement.js
@@ -11,11 +11,28 @@ class JarPlacement{
     get xsearch() {return $('//*[@id="tableu7dr2k7944"]/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div/div[2]')}
     get filterButton(){return $('//*[@id="auto_jw689k3r2f"]/div')}
     get dropdownFilter1(){return $('//*[@id="auto_x99z2etafj"]/div/div/div[2]/span/span/div/button/span')}
-    get mobbi(){return $('/html/body/div[7]/div/div/div/div/div/div[2]/div/div[6]/div/a/div')}
-    get filterButton2(){return $('//*[@id="auto_ufsr1lpse1"]/div/div/div/button')}
-    
-    
-    
+    get mobbi(){return $('body > div.bp3-portal > div > div > div > div > div > div.menu-virtual-list > div > div:nth-child(6) > div')}
+    get filterButton2(){return $('#auto_ufsr1lpse1 > div > div')}
+    get dropdownfilter2() {return $('#auto_ouvc9vif54 > div > div > div.bp3-control-group.bp3-fill.sc-iMSIvx.kxHJWT > span > span > div > button > span')}
+    get dropdownfilter3() {return $('#auto_yfpkzykwxb > div > div > div.bp3-control-group.bp3-fill.sc-iMSIvx.kxHJWT > span > span > div > button > span')}
+    get dropdownfilter4() {return $('#auto_1ndc0m6zex > div > div > div.bp3-control-group.bp3-fill.sc-iMSIvx.kxHJWT > span > span > div > button > span')}
+    get dropdownfilter5() {return $('#auto_rntzji3p1l > div > div > div.bp3-control-group.bp3-fill.sc-iMSIvx.kxHJWT > span > span > div > button > span')}
+    get resname(){return $('body > div.bp3-portal > div > div > div > div > div > div.menu-virtual-list > div > div:nth-child(6)')}
+    get posname(){return $('body > div.bp3-portal > div > div > div > div > div > div.menu-virtual-list > div > div:nth-child(8)')}
+    get typename(){return $('body > div.bp3-portal > div > div > div > div > div > div.menu-virtual-list > div > div:nth-child(3)')}
+    get statusname(){return $('body > div.bp3-portal > div > div > div > div > div > div.menu-virtual-list > div > div:nth-child(1)')}
+    get buttonX1(){return $('[name="cross"]')}
+    get buttonreset(){return $('#auto_vthq3wyg2s > div > div > div')}
+    get buttonxfilter(){return $('#auto_sgwzylx7mt > div > div')}
+    get searchfilter(){return $('body > div.bp3-portal > div > div > div > div > div > div.bp3-input-group > input')}
+    get searchfound(){return $('body > div.bp3-portal > div > div > div > div > div > div.menu-virtual-list > div > div')}
+    get searchfailed(){return $('body > div.bp3-portal > div > div > div > div > div > div.sc-fTrzCy.iLWzlA > a > div')}
+    get closeSearch(){return $('body > div.bp3-portal > div > div > div > div > div > div.bp3-input-group > span.bp3-input-action')}
+    get deleteButton(){return $('#auto_gxywc0gd39 > div > div > span > div > button')}
+    get deleteButton2(){return $('#auto_l3xtpmalus > div > div > span > div > div')}
+    get canceldelete(){return $('#auto_lfcmo5tdcp > div > div > span > div > div')}
+    get viewdetail(){return $('#auto_0k3c5fbdqw > div > div > span > div')}
+    get data1(){return $('//*[@id="tableu7dr2k7944"]/div[2]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[6]/div[1]/div/div/div')}
     
     //action
     async login(username,password){
@@ -46,6 +63,60 @@ class JarPlacement{
     }
     async clickFilterButton2(){
         await this.filterButton2.click()
+    }
+    async clickDropDownFilter2(){
+        await this.dropdownfilter2.click()
+    }
+    async clickDropDownFilter3(){
+        await this.dropdownfilter3.click()
+    }
+    async clickDropDownFilter4(){
+        await this.dropdownfilter4.click()
+    }
+    async clickDropDownFilter5(){
+        await this.dropdownfilter5.click()
+    }
+    async clickResName(){
+        await this.resname.click()
+    }
+    async clickPosName(){
+        await this.posname.click()
+    }
+    async clickTypeName(){
+        await this.typename.click()
+    }
+    async clickstatusname(){
+        await this.statusname.click()
+    }
+    async clickbuttonX1(){
+        await this.buttonX1.click()
+    }
+    async clickbuttonreset(){
+        await this.buttonreset.click()
+    }
+    async clickbuttonXfilter(){
+        await this.buttonxfilter.click()
+    }
+    async isisearchfilter(keyword){
+        await this.searchfilter.setValue(keyword)
+    }
+    async clickCloseSearch(){
+        await this.closeSearch.click()
+    }
+    async clickButtonDelete(){
+        await this.deleteButton.click()
+    }
+    async clickButtonDelete2(){
+        await this.deleteButton2.click()
+    }
+    async clickcanceldelete(){
+        await this.canceldelete.click()
+    }
+    async clickviewdetail(){
+        await this.viewdetail.click()
+    }
+    async clickdata1(){
+        await this.data1.click()
     }
 
     async openPage(){
--- test/specs/fajar_placement.spec.js
+++ test/specs/fajar_placement.spec.js
@@ -6,9 +6,9 @@ describe ('Test Case 1 -24', function(){
         await jarPlacement.openPage()
         await browser.setWindowSize(1920,1080)
         await jarPlacement.login('dummy@prosigmaka.com','dummypsm')
-        await browser.pause(3000)
+        await browser.pause(2000)
         await jarPlacement.openTimesheet()
-        await browser.pause(6000)
+        await browser.pause(2000)
         await jarPlacement.openPlacement()
     })
 
@@ -20,7 +20,7 @@ describe ('Test Case 1 -24', function(){
     })
     it.skip('test case 2 - Searching by Client Name', async function (){
         await jarPlacement.writeSearchBar('Astra International')
-        await expect(jarPlacement.prf1).toHaveText({})
+        await expect(jarPlacement.prf1).toHaveText('{}')
     })
     it.skip('test case 3 - Searching by Resource Name', async function (){
         await jarPlacement.writeSearchBar('testerz')
@@ -38,64 +38,133 @@ describe ('Test Case 1 -24', function(){
     it.skip('test case 6 - Searching by Status', async function (){
         
     })
-    it('test case 7 - Filtered by Client ', async function (){
+    it('test case 7 - Filtered by Client ', async function (){ // PASSED
         await jarPlacement.clickFilterButton()
         await jarPlacement.clickDropDownFilter1()
         await browser.pause(2000)
         await jarPlacement.clickMobbi()
-        await jarPlacement.clickFilterButton2
+        await browser.pause(1000)
+        await jarPlacement.clickFilterButton2()
+        await browser.pause(2000)
         await expect(jarPlacement.prf1).toHaveText('1237')
     })
-    it('test case 8 - Filtered by Resource', async function (){
-        
+    it('test case 8 - Filtered by Resource', async function (){ //passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickbuttonX1()
+        await jarPlacement.clickDropDownFilter2()
+        await browser.pause(2000)
+        await jarPlacement.clickResName()
+        await browser.pause(1000)
+        await jarPlacement.clickFilterButton2()
+        await browser.pause(2000)
+        await expect(jarPlacement.prf1).toHaveText('1237')
     })
-    it('test case 9 - Filtered by Position', async function (){
-        
+    it('test case 9 - Filtered by Position', async function (){//passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickbuttonX1()
+        await jarPlacement.clickDropDownFilter3()
+        await browser.pause(2000)
+        await jarPlacement.clickPosName()
+        await browser.pause(1000)
+        await jarPlacement.clickFilterButton2()
+        await browser.pause(2000)
+        await expect(jarPlacement.prf1).toHaveText('1239')
     })
-    it('test case 10 - Filtered by Type', async function (){
-        
+    it('test case 10 - Filtered by Type', async function (){//passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickbuttonX1()
+        await jarPlacement.clickDropDownFilter4()
+        await jarPlacement.clickTypeName()
+        await jarPlacement.clickFilterButton2()
+        await expect(jarPlacement.prf1).toHaveText('1237')
     })
-    it('test case 11 - Filtered by Status', async function (){
-        
+    it('test case 11 - Filtered by Status', async function (){//passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickbuttonX1()
+        await jarPlacement.clickDropDownFilter5()
+        await jarPlacement.clickstatusname()
+        await jarPlacement.clickFilterButton2()
+        await expect(jarPlacement.prf1).toHaveText('1239')
     })
-    it('test case 12 - Filtered by 2 Condition', async function (){
-        
+    it('test case 12 - Filtered by 2 Condition', async function (){//passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickbuttonX1()
+        await jarPlacement.clickDropDownFilter1()
+        await jarPlacement.clickMobbi()
+        await jarPlacement.clickDropDownFilter2()
+        await jarPlacement.clickResName()
+        await jarPlacement.clickFilterButton2()
+        await expect(jarPlacement.prf1).toHaveText('1237')
     })
-    it('test case 13 - Filtered by 3 Condition', async function (){
-        
+    it('test case 13 - Filtered by 3 Condition', async function (){//passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickDropDownFilter3()
+        await jarPlacement.clickPosName()
+        await jarPlacement.clickFilterButton2()
+        await expect(jarPlacement.prf1).toHaveText('1237')
     })
-    it('test case 14 - Filtered by 4 Condition', async function (){
-        
+    it('test case 14 - Filtered by 4 Condition', async function (){//passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickDropDownFilter4()
+        await jarPlacement.clickTypeName()
+        await jarPlacement.clickFilterButton2()
+        await expect(jarPlacement.prf1).toHaveText('1237')
     })
-    it('test case 15 - Filtered by 5 Condition', async function (){
-        
+    it('test case 15 - Filtered by 5 Condition', async function (){//passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickDropDownFilter5()
+        await jarPlacement.clickstatusname()
+        await jarPlacement.clickFilterButton2()
+        await expect(jarPlacement.prf1).toHaveText('1237')
     })
-    it('test case 16 - Filtered Without Condition', async function (){
-        
+    it('test case 18 - Reset Filter', async function (){//passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickbuttonreset()
+        await expect(jarPlacement.prf1).toHaveText('1239')
+
     })
-    it('test case 17 - Cancelling Filtration', async function (){
-        
+    it('test case 16 - Filtered Without Condition', async function (){//passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickFilterButton2()
+        await expect(jarPlacement.prf1).toHaveText('1239')
     })
-    it('test case 18 - Reset Filter', async function (){
-        
+    it('test case 17 - Cancelling Filtration', async function (){//passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickbuttonXfilter()
+        await expect(jarPlacement.prf1).toHaveText('1239')
     })
-    it('test case 19 - Searching on Filter With Valid Keyword', async function (){
-        
+    it('test case 20 - Searching on Filter With Invalid Keyword ', async function (){//passed
+        await jarPlacement.clickFilterButton()
+        await jarPlacement.clickDropDownFilter1()
+        await jarPlacement.isisearchfilter('mobbo')
+        await expect(jarPlacement.searchfailed).toHaveText('No Results Found')
     })
-    it('test case 20 - Searching on Filter With Invalid Keyword ', async function (){
-        
+    it('test case 19 - Searching on Filter With Valid Keyword', async function (){//passed
+        await jarPlacement.isisearchfilter('mobbi')
+        await expect(jarPlacement.searchfound).toHaveText('Mobbi')
     })
-    it('test case 21 - Delete Search Keyword', async function (){
-        
+    
+    it.skip('test case 21 - Delete Search Keyword', async function (){
+        await jarPlacement.clickCloseSearch()
+        await expect
     })
-    it('test case 22 - Delete Placement' , async function (){
-        
+    it.only('test case 22 - Delete Placement' , async function (){
+        // await jarPlacement.clickbuttonXfilter()
+        await jarPlacement.clickButtonDelete()
+        await jarPlacement.clickButtonDelete2()
+        await expect(jarPlacement.prf1).toHaveText('1239')
+
     })
-    it('test case 23 - Cancel Delete Placement', async function (){
-        
+    it('test case 23 - Cancel Delete Placement', async function (){//passed
+        await jarPlacement.clickButtonDelete()
+        await jarPlacement.clickcanceldelete()
+        await expect(jarPlacement.prf1).toHaveText('123456')
+
     })
-    it('test case 24 - View Detail', async function (){
-        
+    it('test case 24 - View Detail', async function (){//passed
+        await jarPlacement.clickdata1()
+        await jarPlacement.clickviewdetail()
+        await expect (browser).toHaveUrl('https://timesheet.app.prosigmaka.com/app/timesheet-admin/placement-detail-659f5b33d90d8b3224f1c2c4?id=e5750a55-399a-41a4-8968-b2114bdf8803&pageID=Placement%20List')
     })
 
 
